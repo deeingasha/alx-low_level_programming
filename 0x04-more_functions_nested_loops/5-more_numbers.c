53631f245d1983ac09bbc64712c6dce2c56f5b2b
Dorcas <dorcasingasha@gmail.com>
2022-10-21 21:25:11 +0000

    Task 5
    Function that prints numbers 0 to 14, 10 times

---
+++ 0x04-more_functions_nested_loops/5-more_numbers.c
@@ -0,0 +1,22 @@
+#include "main.h"
+
+/**
+  * more_numbers - prints 0 to 14 10 times
+  *
+  * Return: 0
+  */
+void more_numbers(void)
+{
+	int i, times;
+
+	for (times = 0; times <= 10; times++)
+	{
+		for (i = 0; i < 15; i++)
+		{
+			if (i >= 10)
+				_putchar('1');
+			_putchar(i % 10 + '0');
+		}
+		_putchar('\n');
+	}
+}
