5e24d12a3a897e2888cae98cbd5dccd921c47f2e
Dorcas <dorcasingasha@gmail.com>
2022-11-09 20:24:26 +0000

    Task 3
    Function that returns a pointer to a 2 dimensional array of integers.
    Working : prints okay
    Problem : spacing in array

---
+++ 0x0B-malloc_free/3-alloc_grid.c
@@ -0,0 +1,41 @@
+#include "main.h"
+#include <stdlib.h>
+
+/**
+  * alloc_grid - returns a pointer to a 2D array of integers
+  *
+  * @width: width of array
+  * @height: height of array
+  *
+  * Return: pointer to 2D array --> Success
+  *                 NULL--> width or height <= 0
+  */
+int **alloc_grid(int width, int height)
+{
+	int **arr;
+	int i, j;
+
+	if (width <= 0 || height <= 0)
+		return (NULL);
+
+	arr = malloc(sizeof(int *) * height);
+	if (arr == NULL)
+		return (NULL);
+
+	for (i = 0; i < height; i++)
+	{
+		arr[i] = malloc(sizeof(int) * width);
+		if (arr[i] == NULL)
+		{
+			for (j = 0; j < i; i++)
+			{
+				free(arr[i]);
+			}
+			free(arr);
+			return (NULL);
+		}
+		for (j = 0; j < width; j++)
+			arr[i][j] = 0;
+	}
+	return (arr);
+}
