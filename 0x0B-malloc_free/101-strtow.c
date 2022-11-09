452064782d19bc09cca31a58766d33626635851d
Dorcas <dorcasingasha@gmail.com>
2022-11-09 21:40:25 +0000

    First attempt

---
+++ 0x0B-malloc_free/101-strtow.c
@@ -0,0 +1,30 @@
+#include "main.h"
+#include <stdlib.h>
+
+/**
+  * strtow - splits a string into words
+  *
+  * @str: string to split
+  *
+  * Return: Success --> pointer to an array of null-terminated words
+  *           Fail  --> NULL
+  */
+char **strtow(char *str)
+{
+	char **ptr;
+/*	int i;*/
+
+	if (str == NULL || *str == 0)
+		return (NULL);
+
+	/*
+	 * count the  elements in the string
+	 * count the number of words
+	 *
+	 * allocate space with malloc
+	 * separate and store
+	 *
+	 */
+	ptr = malloc(sizeof(char) * 40); /* random to compile*/
+	return (ptr);
+}
