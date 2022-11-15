8ffb96e94c7ae16d5aa73ee10fe8be0b07f1471e
Dorcas <dorcasingasha@gmail.com>
2022-11-15 22:02:33 +0000

    Task 0
    Define a new type struct dog with the following elements:name, age, owner.

---
+++ 0x0E-structures_typedef/dog.h
@@ -0,0 +1,18 @@
+#ifndef DOG_H
+#define DOG_H
+
+/**
+  * struct dog - contains info about dog
+  *
+  * @name: name of dog
+  * @age: age of dog
+  * @owner: owner's name
+  */
+struct dog
+{
+	char *name;
+	float age;
+	char *owner;
+};
+
+#endif /* DOG_H */
