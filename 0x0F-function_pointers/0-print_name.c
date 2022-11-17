096b280c114996f5bf143279f3269c8608e0b646
Dorcas <dorcasingasha@gmail.com>
2022-11-17 15:58:13 +0000

    Improve readability

--- 0x0F-function_pointers/0-print_name.c
+++ 0x0F-function_pointers/0-print_name.c
@@ -1,5 +1,5 @@
 #include "function_pointers.h"
-#include <stdio.h>
+
 /**
   * print_name - prints a name
   *
@@ -10,6 +10,6 @@
   */
 void print_name(char *name, void (*f)(char *))
 {
-	if (name && f)
+	if (name != NULL && f != NULL)
 		f(name);
 }
