3b29753b78624f5e70b946eed91dcc64ef1a111b
Dorcas <dorcasingasha@gmail.com>
2022-11-24 14:33:59 +0000

    Deal with NULL ptr

--- 0x0C-more_malloc_free/100-realloc.c
+++ 0x0C-more_malloc_free/100-realloc.c
@@ -16,7 +16,13 @@ void *_realloc(void *ptr, unsigned int old_size, unsigned int new_size)
 
 	if (old_size == new_size)
 		return (ptr);
-	if (new_size == 0 && ptr != NULL)
+	if (ptr == NULL)
+	{
+		p = malloc(new_size);
+		if (p == NULL)
+			return (NULL);
+	}
+	else if (new_size == 0)
 	{
 		free(ptr);
 		return (NULL);
