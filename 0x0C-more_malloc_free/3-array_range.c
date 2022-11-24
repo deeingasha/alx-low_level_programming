7fe01c86e2e9a08bbe2a3d792bb9426942649feb
Dorcas <dorcasingasha@gmail.com>
2022-11-24 13:42:29 +0000

    Task 3
    Function that creates an array of integers ordered from min to max

---
+++ 0x0C-more_malloc_free/3-array_range.c
@@ -0,0 +1,32 @@
+#include "main.h"
+
+/**
+  * array_range - creates and array of integers ordered from min to max
+  *
+  * @min: smallest int
+  * @max: largest int
+  *
+  * Return: pointer to the ordered array
+  *           NULL --> malloc failed or min > max
+  */
+int *array_range(int min, int max)
+{
+	int i;
+	int len;
+	int *arr;
+
+	if (min > max)
+		return (NULL);
+
+	len = max - min + 1;
+	arr = malloc(sizeof(int) * len);
+	if (arr == NULL)
+		return (NULL);
+
+	for (i = min; i <= max; i++)
+	{
+		arr[i] = min;
+		min++;
+	}
+	return (arr);
+}
