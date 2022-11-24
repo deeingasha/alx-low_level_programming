c407b8ec147b7f4180f78d5d08587194b6be6ae8
Dorcas <dorcasingasha@gmail.com>
2022-11-24 14:28:06 +0000

    Task 4
    Function that reallocates a memory block using malloc and free

---
+++ 0x0C-more_malloc_free/100-realloc.c
@@ -0,0 +1,33 @@
+#include "main.h"
+
+/**
+  * _realloc - allocates a block of memory using malloc and free
+  *
+  * @ptr: pointer to memory previously allocated by malloc
+  * @old_size: size in bytes of space allocated for ptr
+  * @new_size: new size to allocate
+  *
+  * Return: NULL --> new_size == 0 && ptr != NULL
+  */
+void *_realloc(void *ptr, unsigned int old_size, unsigned int new_size)
+{
+	unsigned int i;
+	char *p;
+
+	if (old_size == new_size)
+		return (ptr);
+	if (new_size == 0 && ptr != NULL)
+	{
+		free(ptr);
+		return (NULL);
+	}
+
+	p = malloc(new_size);
+	if (ptr == NULL)
+		return (NULL);
+	for (i = 0; i < new_size && i < old_size; i++)
+		p[i] = *((char *)ptr + i);/*can't use void pointer in arithmetic*/
+	free(ptr);
+	p = (void *)p;
+	return (p);
+}
